5ffe95cb7abf8a91dcb3fd1a9cedf344120ff16b
8b1tz <yohannaoliveira12@gmail.com>
2021-02-27 18:15:24 -0300

    Arrumando consoles

--- src/aplicacao_console/Atualizar.java
+++ src/aplicacao_console/Atualizar.java
@@ -1,5 +1,23 @@
 package aplicacao_console;
 
+import fachada.Fachada;
+
 public class Atualizar {
 
+	public Atualizar() {
+		Fachada.inicializar();
+		try {
+			Fachada.adicionarAssunto("aa", "Felipe Neto");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		Fachada.finalizar();
+		System.out.println("fim do programa");
+	}
+
+	// =================================================
+	public static void main(String[] args) {
+		new Atualizar();
+	}
 }
